14b4b634d11ad6cc66ae834a95631232f91a5860
Alberto G. Catalán <albertogonzalezcatalan@gmail.com>
2014-12-18 19:33:42 +0100

    CLI updated

--- cli/clibootstrap.php
+++ cli/clibootstrap.php
@@ -7,14 +7,13 @@
 
 date_default_timezone_set("Europe/Madrid");
 
-ini_set('display_errors',1);
+ini_set('display_errors', 1);
 error_reporting(E_ALL|E_STRICT);
-ini_set('error_log','script_errors.log');
-ini_set('log_errors','On');
+ini_set('error_log', 'script_errors.log');
+ini_set('log_errors', 'On');
 
 define('GLOBAL_PATH', dirname( __FILE__ ));
-define('MODEL_PATH', GLOBAL_PATH . '/../Quaver/Model');
-define('VIEW_PATH', GLOBAL_PATH . '/../Quaver/View');
+define('MODEL_PATH', GLOBAL_PATH . '/../Quaver/App/Model');
 define('CONTROLLER_PATH', GLOBAL_PATH . '/../Quaver/Controller');
 
 
